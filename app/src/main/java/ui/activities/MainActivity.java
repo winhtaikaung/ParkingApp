79003f5a73b306640170d220bc2faf4ff87e2937
ဝင်းထိုက်အောင် <winhtaikaung28@hotmail.com>
2016-02-27 11:29:23 +0800

    Tab Layout Finished

--- app/src/main/java/ui/activities/MainActivity.java
+++ app/src/main/java/ui/activities/MainActivity.java
@@ -1,9 +1,14 @@
 package ui.activities;
 
+import android.graphics.Color;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.widget.TextView;
 
 import com.parking.R;
 
@@ -11,19 +16,42 @@ import API.RetrofitAPI;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
+import ui.adapters.ViewPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    String[] titles={"FAVOURITES","PARKINGS"};
+    private int[] tabIcons = {
+            R.drawable.ic_fav,
+            R.drawable.ic_parking
+
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         toolbar=(Toolbar) findViewById(R.id.toolbar);
+        tabLayout=(TabLayout) findViewById(R.id.tabs);
+        viewPager=(ViewPager) findViewById(R.id.pager);
+
+
+
         setSupportActionBar(toolbar);
-        if(getSupportActionBar()==null){
-            getSupportActionBar().setTitle("LOL");
-        }
+
+
+        final  ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(),titles);
+        pager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(pager);
+
+
+        setupTabIcons();
+
+
+
 
         RetrofitAPI.getInstance(this).getService().getParkings(new Callback<String>() {
             @Override
@@ -37,4 +65,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void setupTabIcons() {
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_fav);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_parking);
+
+
+    }
+
+
 }
--- app/src/main/java/ui/fragments/ParkingFragment.java
+++ app/src/main/java/ui/fragments/ParkingFragment.java
@@ -1,9 +1,23 @@
 package ui.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.parking.R;
 
 /**
  * Created by winhtaikaung on 2/26/16.
  */
 public class ParkingFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.fragment_parking,container,false);
+        return v;
+    }
 }
