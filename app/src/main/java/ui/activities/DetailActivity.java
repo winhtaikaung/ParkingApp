f1c8f664d36c19265d9b29a5aa535b635b8b6a7b
ဝင်းထိုက်အောင် <winhtaikaung28@hotmail.com>
2016-03-01 08:42:37 +0800

    detail _branchadded

---
+++ app/src/main/java/ui/activities/DetailActivity.java
@@ -0,0 +1,27 @@
+package ui.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.parking.R;
+
+import ui.fragments.DetailViewFragment;
+
+/**
+ * Created by winhtaikaung on 3/1/16.
+ */
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        if(savedInstanceState==null){
+            getSupportFragmentManager().beginTransaction().add(R.id.content, new DetailViewFragment()).commit();
+        }
+
+
+    }
+}
---
+++ app/src/main/java/ui/fragments/DetailViewFragment.java
@@ -0,0 +1,78 @@
+package ui.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.parking.R;
+
+/**
+ * Created by winhtaikaung on 3/1/16.
+ */
+public class DetailViewFragment extends Fragment implements OnMapReadyCallback {
+    SupportMapFragment map;
+    GoogleApiClient mGoogleApiClient;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.fragment_detail,container,false);
+
+            buildGoogleApiClient();
+        map=(SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        map.getMapAsync(this);
+
+
+        return v;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(41.889, -87.622), 16));
+
+        // You can customize the marker image using images bundled with
+        // your app, or dynamically generated bitmaps.
+        map.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_icon))
+                .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+                .position(new LatLng(41.889, -87.622)));
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(Bundle bundle) {
+
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+                        mGoogleApiClient.connect();
+                    }
+                })
+                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+                    }
+                })
+
+                .addApi(LocationServices.API).build();
+    }
+}
